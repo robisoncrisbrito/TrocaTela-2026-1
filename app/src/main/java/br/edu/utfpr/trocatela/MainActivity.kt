63630ed3748bb8a09ceb3bf43b3253f415f2e1de
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2026-04-08 19:29:00 -0300

    Aplicativo troca tela com a primeira tela pronta (menu inicial)

---
+++ app/src/main/java/br/edu/utfpr/trocatela/MainActivity.kt
@@ -0,0 +1,26 @@
+package br.edu.utfpr.trocatela
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun btLancamentoOnClick(view: View) {
+        Toast.makeText(this, "Lançamento", Toast.LENGTH_SHORT).show()
+    }
+}
