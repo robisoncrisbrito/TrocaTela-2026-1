fe0b04cf9c88740013dab20d3aecc6b15ee53e80
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2026-04-08 20:15:22 -0300

    Tela do aplicativo trocatela, já com a terceira tela de ConfirmarActivity

--- app/src/main/java/br/edu/utfpr/trocatela/ConfirmarActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela/ConfirmarActivity.kt
@@ -1,29 +1,38 @@
 package br.edu.utfpr.trocatela
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ConfirmarActivity : AppCompatActivity() {
+
+    private lateinit var tvCod: TextView
+    private lateinit var tvQtd: TextView
+    private lateinit var tvValor: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_confirmar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        tvCod = findViewById(R.id.tvCod)
+        tvQtd = findViewById(R.id.tvQtd)
+        tvValor = findViewById(R.id.tvValor)
+
+        tvCod.text = intent.getStringExtra("cod")
+        tvQtd.text = intent.getStringExtra("qtd")
+        tvValor.text = intent.getStringExtra("valor")
+
     }
 
-    fun btLancamentoOnClick(view: View) {
-        val intent = Intent(this, LancamentoActivity::class.java)
-        startActivity(intent)
-    }
+    fun btEnviarMsgOnClick(view: View) {}
 }
--- app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
@@ -1,13 +1,20 @@
 package br.edu.utfpr.trocatela
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LancamentoActivity : AppCompatActivity() {
+
+    private lateinit var etCod: EditText
+    private lateinit var etQtd: EditText
+    private lateinit var etValor: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,11 +24,20 @@ class LancamentoActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        etCod = findViewById(R.id.etCod)
+        etQtd = findViewById(R.id.etQtd)
+        etValor = findViewById(R.id.etValor)
     }
 
     fun btConfirmarOnClick(view: View) {
-
+        val intent = Intent(this, ConfirmarActivity::class.java)
+        intent.putExtra("cod", etCod.text.toString())
+        intent.putExtra("qtd", etQtd.text.toString())
+        intent.putExtra("valor", etValor.text.toString())
+        startActivity(intent)
     }
+
     fun btListarOnClick(view: View) {
 
     }
